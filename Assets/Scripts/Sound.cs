c8e069542bb8adedcc9fa111dcc3d8905c691f0a
Zach 🦀🌲 <zachary.jay.crabtree@gmail.com>
2022-01-25 19:58:08 -0800

    Add basic piano and synth sounds and map volume to velocity

--- Assets/Scripts/Sound.cs
+++ Assets/Scripts/Sound.cs
@@ -9,31 +9,58 @@ public class Sound : MonoBehaviour {
     private AudioMixer _audioMixer;
     private AudioMixerGroup _masterAudioMixerGroup;
     
-    public List<AudioClip> ambienceClips;
-    public AudioSource ambienceSource;
+    public List<AudioClip> natureClips;
+    public AudioSource natureSource;
+    
+    public List<AudioClip> pianoClips;
+    public AudioSource pianoSource;
+    
+    public List<AudioClip> synthClips;
+    public AudioSource synthSource;
     void Start() {
         _datastore = GetComponent<Datastore>();
         _audioMixer = Resources.Load<AudioMixer>("MainMixer");
         _masterAudioMixerGroup = _audioMixer.FindMatchingGroups("Master")[0];
         
-        (ambienceSource, ambienceClips) = loadClips(1, "Sounds/station_ambience_");
+        (natureSource, natureClips) = loadClips(1, "Sounds/station_ambience_");
+        (pianoSource, pianoClips) = loadClips(1, "Sounds/piano_ambience_");
+        (synthSource, synthClips) = loadClips(1, "Sounds/synth_ambience_");
         
-        ambienceSource.gameObject.name = "Ambience source";
-        ambienceSource.volume = 0.2f;
-        ambienceSource.clip = ambienceClips.Single();
-        ambienceSource.loop = true;
-        ambienceSource.Play();
+        natureSource.gameObject.name = "Ambience source";
+        natureSource.volume = 0.2f;
+        natureSource.clip = natureClips.Single();
+        natureSource.loop = true;
+        natureSource.Play();
+        
+        pianoSource.gameObject.name = "Piano source";
+        pianoSource.volume = 0.2f;
+        pianoSource.clip = pianoClips.Single();
+        pianoSource.loop = true;
+        pianoSource.Play();
+        
+        synthSource.gameObject.name = "Synth source";
+        synthSource.volume = 0.2f;
+        synthSource.clip = synthClips.Single();
+        synthSource.loop = true;
+        synthSource.Play();
     }
 
     void FixedUpdate() {
         var distance = Math.Min(_datastore.distFromLastStation.Value, _datastore.distToNextStation.Value);
         if (distance > 100) {
-            ambienceSource.volume = 0;
+            natureSource.volume = 0;
         }
         else {
-            ambienceSource.volume = (float) (-Math.Min(Math.Abs(distance), 100) + 100) / 100;    
+            natureSource.volume = (float) (-Math.Min(Math.Abs(distance), 100) + 100) / 100;
         }
-        
+        synthSource.volume = Math.Min(0.3f, 0.3f - natureSource.volume);
+        pianoSource.volume = synthSource.volume +
+                             synthSource.volume *
+                             (float)((Math.Min(Math.Abs(_datastore.train.velocity),
+                                          _datastore.train.maxVelocity) -
+                                      _datastore.train.maxVelocity) /
+                                     _datastore.train.maxVelocity);
+
     }
     
     (AudioSource, List<AudioClip>) loadClips(int numClips, string pathPrefix, string numSuffixLength="D2", int startIndex=1) {
